26e354066fd30e0efda96ce9ad2bc8ee9228674f
capsaicin01 <tuna4-2@hotmail.com>
2023-01-04 17:59:52 +0300

    Added lots of music player features

--- MusicClasses/MusicClasses.py
+++ MusicClasses/MusicClasses.py
@@ -4,6 +4,25 @@ import youtube_dl
 
 class Song(dict):
 
+    """
+    This is a class for creating a song object from an url or a song name.
+
+    Attributes:
+        url (str): Mp3 file url of the song.
+        title (str): Title of the song.
+        thumbnail (str): Thumbnail url of the song.
+        channel (str): Channel name of the song uploader.
+        duration_raw (int): Duration of the song by seconds.
+        duration_formatted (str): Duration of the song as '<minutes>m <seconds>s'.
+        upload_date_raw (int): Upload date of the song as 'yyyymmdd'.
+        upload_date_formatted (str): Upload date of the song as 'dd/mm/yyyy'.
+        views (int): Number of views of the song.
+        requested_by (discord.Member): User who requested the song.
+
+    """
+
+
+
     YDL_OPTIONS = {
         'format': 'bestaudio/best',
         'noplaylist': True,
@@ -29,6 +48,7 @@ class Song(dict):
 
             self.update(info)
             self["requested_by"] = author
+            print(self)
 
     @property
     def url(self):
@@ -43,8 +63,8 @@ class Song(dict):
         return self.get("thumbnail")
 
     @property
-    def description(self):
-        return self.get("description")
+    def channel(self):
+        return self.get("channel")
 
     @property
     def duration_raw(self):
@@ -66,5 +86,45 @@ class Song(dict):
         return f"{d}/{m}/{y}"
 
     @property
+    def views(self):
+        return self.get("view_count")
+
+    @property
     def requested_by(self):
         return self.get("requested_by")
+
+
+class Queue(list):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._currentSong = None
+
+    def next_song(self):
+        next_one = self.pop(0)
+        self._currentSong = next_one
+        return self._currentSong
+
+    def clear(self):
+        super().clear()
+        self._currentSong = None
+    
+    def get_embed(self, song_index=0):
+        
+        if song_index <= 0:
+            song = self._currentSong
+        else:
+            song = self[song_index - 1]
+        
+        embed = discord.Embed()
+        embed.color = discord.Color.teal()
+        embed.title = "Song Info"
+        
+        embed.set_thumbnail(url=song.thumbnail)
+        embed.add_field(name="Title", value = song.title)
+        embed.add_field(name="Channel", value = song.channel)
+        embed.add_field(name="Duration", value = song.duration_formatted)
+        embed.add_field(name="Upload Date", value = song.upload_date_formatted)
+        embed.add_field(name="Views", value = song.views)
+        embed.add_field(name="Requested By", value = song.requested_by.mention)
+
+        return embed
--- cogs/MusicCog.py
+++ cogs/MusicCog.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.utils import get
+import asyncio
 import youtube_dl
 from MusicClasses.MusicClasses import *
 
@@ -8,6 +9,7 @@ class MusicCog(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+        self.music_queues = {}
 
     @commands.command()
     async def join(self, ctx):
@@ -21,13 +23,18 @@ class MusicCog(commands.Cog):
     
     @commands.command()
     async def play(self, ctx, url:str, *args):
+
+        if not ctx.guild.id in self.music_queues:
+            self.music_queues.update({ctx.guild.id: Queue()})
+            print(self.music_queues)
+
+        voice = get(self.client.voice_clients, guild=ctx.guild)
+        queue = self.music_queues[ctx.guild.id]
+
         voice_channel = ctx.author.voice.channel
         if ctx.author.voice is None:
             await ctx.send("You are not in a voice channel")
             return
-        
-        if not self.client_in_same_channel(ctx.author, ctx.guild):
-            await voice_channel.connect()
         
         if not url.startswith("https://"):
             url = f"ytsearch: {url} {' '.join(args)}"
@@ -35,15 +42,63 @@ class MusicCog(commands.Cog):
         song = Song(url, ctx.author)
         voice = get(self.client.voice_clients, guild=ctx.guild)
 
-        voice.play(discord.FFmpegPCMAudio(song.url))
-        await ctx.send(f"Now playing: ***{song.title}***")
+        queue.append(song)
+        await ctx.send(f"***{song.title}*** has been added to the queue.")
+        
+        if not self.is_in_same_channel(ctx.author, ctx.guild):
+            await voice_channel.connect()
+        
+        await self.play_queue(ctx.guild)
+        await self.inactivity_disconnect(ctx.guild)
 
     @commands.command()
     async def stop(self, ctx):
         voice = get(self.client.voice_clients, guild=ctx.guild)
         voice.stop()
+    
+    @commands.command()
+    async def songinfo(self, ctx, song_idx = None):
+        queue = self.music_queues[ctx.guild.id]
+        embed = queue.get_embed()
+        await ctx.send(embed=embed)
 
-    def client_in_same_channel(self, author:discord.Member, guild:discord.Guild):
+    async def play_song(self, guild:discord.Guild, song:Song):
+        voice = get(self.client.voice_clients, guild=guild)
+        voice.play(discord.FFmpegPCMAudio(song.url))
+
+    async def wait_for_end_of_song(self, guild:discord.Guild):
+        voice = get(self.client.voice_clients, guild=guild)
+        while voice.is_playing():
+            await asyncio.sleep(3)
+
+    async def play_queue(self, guild:discord.Guild):
+
+        queue = self.music_queues[guild.id]
+
+        while True:
+            try:
+                await self.wait_for_end_of_song(guild)
+                song = queue.next_song()
+                await self.play_song(guild, song)
+            except:
+                break
+
+    async def inactivity_disconnect(self, guild:discord.Guild):
+        client_channel = guild.voice_client.channel
+
+        while True:
+            if len(client_channel.members) == 1:
+                await self.leave_voice(guild)
+            await asyncio.sleep(60)
+            
+
+    async def leave_voice(self, guild:discord.Guild):
+        voice = get(self.client.voice_clients, guild=guild)
+        await voice.disconnect()
+        self.music_queues[guild.id].clear()
+
+
+    def is_in_same_channel(self, author:discord.Member, guild:discord.Guild):
         voice = get(self.client.voice_clients, guild=guild)
         user_channel = author.voice.channel
 
